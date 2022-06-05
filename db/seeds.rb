979e6a237b0234dffe1938f360bf01905bb48921
erik b <erik.be@outlook.com>
2022-06-05 14:53:35 +0800

    added model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'date'
+
+Pet.create(name:"Hari", address:"Whatev Lu", species:"dog", found_on:Date.today)
